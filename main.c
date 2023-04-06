d14841669eb84a17f3568c5cfaf06af8e0d7bb0d
mle-boud <mewen.le.boudec@gmail.com>
2023-04-06 14:01:29 +0200

    update last changes in .h

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mle-boud <mle-boud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 20:26:36 by mle-boud          #+#    #+#             */
-/*   Updated: 2023/04/06 13:48:55 by mle-boud         ###   ########.fr       */
+/*   Updated: 2023/04/06 13:58:33 by mle-boud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,11 @@ int	main(int ac, char **av, char **env)
 		cmds = parse_line(line);
 		if (!cmds)
 		{
-			free_arr(cmds);
+			free_cmds(cmds);
 			continue ;
 		}
 		exec_cmds(&top_envp, cmds);
-		free_arr(cmds);
+		free_cmds(cmds);
 	}
 	return (free_all(&top_envp, cmds, line), 0);
 }
--- srcs/free_all.c
+++ srcs/free_all.c
@@ -6,7 +6,7 @@
 /*   By: mle-boud <mle-boud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 20:03:03 by mle-boud          #+#    #+#             */
-/*   Updated: 2023/04/05 18:29:39 by mle-boud         ###   ########.fr       */
+/*   Updated: 2023/04/06 14:00:19 by mle-boud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,22 @@ void	free_arr(char **arr)
 	arr = NULL;
 }
 
-void	free_all(t_list	**env, char **parse_cmd, char *line)
+void	free_cmds(t_cmd **cmds)
+{}
+
+void	free_all(t_list	**env, t_cmd **cmds, char *line)
 {
 	rl_clear_history();
 	ft_lstclear(env);
-	free_arr(parse_cmd);
+	free_cmds(cmds);
 	free(line)
 }
 
-void	ft_quit(t_list	**env, char **parse_cmd, int status)
+void	ft_quit(t_list	**env, t_cmd **cmds, int status)
 {
 	rl_clear_history();
 	ft_lstclear(env);
-	free_arr(parse_cmd);
+	free_cmds(cmds);
 	ft_printf("exit\n");
 	exit(status);
 }
